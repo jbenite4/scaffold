36e8686e8333c563c532801020b0bf8c22f3028a
michinbenitez <michinbenitez@hotmai.com>
2012-03-23 15:39:09 -0500

    oelo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sca::Application.config.secret_token = 'fbb8c714e4ac0cc434f466d7a27bcb24c7e721759b4d0b8a140c16ab90bef2ab80ad078a109fc538e8922767612735c5772380e95eab9bebf06af15684272cd5'
